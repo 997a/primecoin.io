25a2d5602716131ad64f4e2d1f82635c30eda537
cashin <cashin5210@163.com>
2018-09-10 18:52:38 +0800

    第一版测试代码

---
+++ js/public.js
@@ -0,0 +1,38 @@
+
+
+$(function(){
+    var imgSrcArr = [
+        './images/1.png',
+        './images/2.png',
+        './images/3.png',
+        './images/4.png',
+        './images/5.png',
+        './images/6.png',
+        './images/7.png',
+        './images/8.png',
+        './images/9.png',
+        './images/10.png',
+        './images/11.png',
+        './images/12.png'
+    ];
+
+    var imgWrap = [];
+
+    function preloadImg(arr) {
+        for(var i =0; i< arr.length ;i++) {
+            imgWrap[i] = new Image();
+            imgWrap[i].src = arr[i];
+        }
+    }
+
+//或者延迟的文档加载完毕在加载图片
+
+    preloadImg(imgSrcArr);
+    // 默认显示
+    $(".dropdown-toggle").dropdown('toggle');
+    $('#toConnect').click(function (){
+        $('.navbar-light .navbar-nav .active>.nav-link').css('color','#fff');
+        $('#toConnect').css('color','#0fd6f3');
+    });
+});
+
